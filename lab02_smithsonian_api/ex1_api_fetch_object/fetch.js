c403cc2e51ecb118556b9d37d7fefee401b10874
madhat5 <davidmalary@gmail.com>
2020-09-15 08:24:15 -0400

    lab2 ex1 done

--- lab02_smithsonian_api/ex1_api_fetch_object/fetch.js
+++ lab02_smithsonian_api/ex1_api_fetch_object/fetch.js
@@ -3,28 +3,29 @@
 
 
 // put your API key here;
-const apiKey = "";  
+const apiKey = "dnOFu3sIplmk8ECoTjSqVINpMAVHNqUPrC6AIvbK";
 
 // Access to individual objects by ID
 const objectBaseURL = "https://api.si.edu/openaccess/api/v1.0/content/";
 
 //fetches content based on id of an object.
 function fetchContentDataById(id) {
-  let url = objectBaseURL + id + "?api_key="+apiKey;
+  let url = objectBaseURL + id + "?api_key=" + apiKey;
   window
-  .fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    console.log("Here's the content data of the specified object:", data.response);
-  })
-  .catch(error => {
-    console.log(error);
-  })
+    .fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      let dataRes = data.response
+
+      console.log("Here's the content data of the specified object:", dataRes);
+    })
+    .catch(error => {
+      console.log(error);
+    })
 }
 
-fetchContentDataById("edanmdm:NMAI_270941");
+fetchContentDataById("edanmdm:nasm_A20100238000");
 
 // Task 1: Find a different object on https://collections.si.edu/search/ and retrieve the data with the code above
 // Make sure to check the box "Only return results with CC0 media" when searching
 // Task 2: Write the result into a variable and explore different variables through the Console
-
